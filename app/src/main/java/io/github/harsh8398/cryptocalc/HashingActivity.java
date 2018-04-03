c6e7ba1f20bfa7e26653b13e80642f14d0fec5e2
Harsh Patel <pharsh58@gmail.com>
2018-04-04 00:20:25 +0530

    added basic activities

--- app/src/main/java/io/github/harsh8398/cryptocalc/HashingActivity.java
+++ app/src/main/java/io/github/harsh8398/cryptocalc/HashingActivity.java
@@ -1,13 +1,39 @@
 package io.github.harsh8398.cryptocalc;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class HashingActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hashing);
+
+        toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setTitle("Hashing");
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
